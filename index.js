7cb1bc5727fb525c007825bf120e1cb7cec0928c
mariovaquero <mvaquero143@gmail.com>
2023-08-21 18:14:41 -0400

    Completed Calculator Challenge

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b)
+{
+    return(a + b);
+
+}
+function subtract(a,b)
+{
+    return(a-b);
+}
+function multiply(a,b)
+{
+    return(a*b);
+}
+function divide(a,b)
+{
+    return(a/b);
+}
+function increment(n)
+{
+    return(n += 1);
+}
+function decrement(n)
+{
+    return(n -= 1);
+}
+function makeInt(value)
+{
+    
+    return parseInt(value,10);
+
+}
+function preserveDecimal(value)
+{
+
+    return parseFloat(value);
+}
